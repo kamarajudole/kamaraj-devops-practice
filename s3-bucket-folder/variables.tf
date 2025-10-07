e49bcf0326edc0b490cf3a6d34a38a76ea678f64
kamarajudole <kamarajudole87@gmail.com>
2025-10-07 13:50:04 +0530

    Update variables.tf

--- s3-bucket-folder/variables.tf
+++ s3-bucket-folder/variables.tf
@@ -1,11 +1,7 @@
 variable "region" {
-  description = "AWS region"
-  type        = string
-  default     = "ap-south-1"
+  default = "us-east-1"
 }
 
 variable "bucket_name" {
-  description = "S3nimda (must be globally unique)"
-  type        = string
-  default     = "kamaraj-terraform-s3-<replace-with-unique-suffix>"
+  default = "kamaraj-terraform-s3-20251007-01"
 }
