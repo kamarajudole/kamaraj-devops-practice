365772064f946e8905ea82f73ddbc85920efec12
kamarajudole <kamarajudole87@gmail.com>
2025-10-07 11:14:32 +0530

    Create variables.tf

---
+++ s3-bucket-folder/variables.tf
@@ -0,0 +1,11 @@
+variable "region" {
+  description = "AWS region"
+  type        = string
+  default     = "ap-south-1"
+}
+
+variable "bucket_name" {
+  description = "S3 bucket name (must be globally unique)"
+  type        = string
+  default     = "kamaraj-terraform-s3-<replace-with-unique-suffix>"
+}
