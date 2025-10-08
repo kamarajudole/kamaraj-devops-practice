bd7976b5eb47b763ffc33836ad41565feb6481a5
kamarajudole <kamarajudole87@gmail.com>
2025-10-08 15:59:28 +0530

    Update variables.tf

--- s3-bucket-folder/variables.tf
+++ s3-bucket-folder/variables.tf
@@ -1,9 +1,7 @@
 variable "region" {
-  description = "AWS region for the S3 bucket"
-  default     = "us-east-1"
+  default = "us-east-1"
 }
 
 variable "s3_bucket_name" {
-  description = "Globally unique name for S3 bucket"
-  default     = "kamaraj-terraform-s3-20251008-01"
+  default = "kamaraj-terraform-s3-20251008" # must be globally unique
 }
